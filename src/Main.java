87a39ee2d40667bb1e52c439607f05035b6ad46b
Edvinas82 <ziukoslenis@gmail.com>
2023-01-05 10:47:55 +0200

    Sukurtas projektas

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        isvestSkaiciusNuo20Iki37();
+    }
+
+    public static void isvestSkaiciusNuo20Iki37 (){
+        for (int i = 20; i < 37; i++) {
+            System.out.println(i);
+        }
+    }
+}
