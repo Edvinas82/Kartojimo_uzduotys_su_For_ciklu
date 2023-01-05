76ffec1182b317453776619afede0a21004cf7f0
Edvinas82 <ziukoslenis@gmail.com>
2023-01-05 11:08:03 +0200

    Pabaigiau antrą Kartojimo užduotį su For ciklu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         isvestSkaiciusNuo20Iki37();
+        isvestiBruksnelius();
+        isvestiSkaicisiusNuo15IkiMinus21();
     }
 
     public static void isvestSkaiciusNuo20Iki37 (){
         for (int i = 20; i < 37; i++) {
             System.out.println(i);
         }
+    }
+    public static void isvestiSkaicisiusNuo15IkiMinus21 (){
+        for (int i = 15; i > -21; i--){
+            System.out.println(i);
+        }
+    }
+    public static void  isvestiBruksnelius (){
+        System.out.println("------------------------");
     }
 }
